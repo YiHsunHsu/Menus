9d4bf7e0e0181749689a2b7b8b3eeb2e267d0a14
Eason_Hsu <hsunslife@gmail.com>
2017-05-16 13:40:13 +0800

    新增 Registration 信箱註冊頁面

--- app/src/main/java/com/example/eason_hsu/menus/LoginActivity.java
+++ app/src/main/java/com/example/eason_hsu/menus/LoginActivity.java
@@ -1,13 +1,53 @@
 package com.example.eason_hsu.menus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private static final int REGISTRATION_FUC =1;
+
+    private EditText editTextLoginEmail;
+    private EditText editTextLoginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextLoginEmail = (EditText)findViewById(R.id.editTextLoginEmail);
+        editTextLoginPassword = (EditText)findViewById(R.id.editTextLoginPassword);
+    }
+
+    public void email_login(View view) {
+        Toast.makeText(this, "email_longin CLICK!", Toast.LENGTH_LONG).show();
+    }
+    public void registration(View view) {
+        startActivityForResult(new Intent(this, RegistrationActivity.class), REGISTRATION_FUC);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode)
+        {
+            case REGISTRATION_FUC:
+                if(resultCode == RESULT_OK){
+                    String email = data.getStringExtra("USEREMAIL");
+                    String password = data.getStringExtra("USERPASSWORD");
+                    editTextLoginEmail.setText(email);
+                    editTextLoginPassword.setText(password);
+                    Log.d("LoginActivity", "onActivityResult -> FUNC_REGISTRATION : USEREMAIL = " + email + "USERPASSWORD = " + password);                }
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/eason_hsu/menus/RegistrationActivity.java
@@ -0,0 +1,76 @@
+package com.example.eason_hsu.menus;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+
+
+    private EditText editTextRegistrationEmail;
+    private EditText editTextNickname;
+    private EditText editTextRegistrationPassword;
+    private EditText editTextConfirmPassword;
+
+    private String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        editTextRegistrationEmail = (EditText)findViewById(R.id.editTextRegistrationEmail);
+        editTextNickname = (EditText)findViewById(R.id.editTextNickname);
+        editTextRegistrationPassword = (EditText)findViewById(R.id.editTextRegistrationPassword);
+        editTextConfirmPassword = (EditText)findViewById(R.id.editTextConfirmPassword);
+    }
+    public void createUser(View view){
+        final String email = editTextRegistrationEmail.getText().toString();
+        final String nickname = editTextNickname.getText().toString();
+        final String password = editTextRegistrationPassword.getText().toString();
+        String confirmPassword = editTextConfirmPassword.getText().toString();
+
+        if(password.equals(confirmPassword)) {
+            final ProgressDialog progressDialog = ProgressDialog.show(RegistrationActivity.this, "請稍後", "帳號建立中", true);
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("Registration", "onComplete yes");
+                                Toast.makeText(RegistrationActivity.this, "帳號建立完成", Toast.LENGTH_LONG).show();
+                                getIntent().putExtra("USEREMAIL", email);
+                                getIntent().putExtra("USERPASSWORD", password);
+                                setResult(RESULT_OK, getIntent());
+                                finish();
+                            } else {
+                                Log.w("Registration", "ERROR", task.getException());
+                                Toast.makeText(RegistrationActivity.this, "註冊失敗", Toast.LENGTH_LONG).show();
+                            }
+                            progressDialog.dismiss();
+                        }
+                    });
+        } else {
+            editTextRegistrationPassword.setText(null);
+            editTextConfirmPassword.setText(null);
+            Toast.makeText(RegistrationActivity.this, "請確認密碼欄與確認碼欄是否一致", Toast.LENGTH_LONG).show();
+        }
+    }
+}
